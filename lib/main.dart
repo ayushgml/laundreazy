d4c9e5f4f11a55dab1687cc8e7735bca34648eec
Ayush <ayush.gupta2020c@vitstudent.ac.in>
2022-10-01 11:38:27 +0530

    firebase auth for android and ios dependecies

--- lib/main.dart
+++ lib/main.dart
@@ -185,8 +185,8 @@ class UserDirection extends StatelessWidget {
               color: Color.fromRGBO(84, 186, 185, 1),
             ),
             child: GestureDetector(
-              // onTap: () => Navigator.of(context).push(MaterialPageRoute(
-              //     builder: (context) => const StudentLogin())),
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => StudentLogin())),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: const [
@@ -230,148 +230,205 @@ class ManagerLogin extends StatelessWidget {
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/image/manager_bg.png'),
-              fit: BoxFit.fill,
-            ),
+          image: DecorationImage(
+            image: AssetImage('assets/image/manager_bg.png'),
+            fit: BoxFit.fill,
           ),
-      padding: const EdgeInsets.all(20),
-      child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'LAUNDRY/IRON MANAGER', textAlign: TextAlign.left, style: TextStyle(
-              color: Color.fromRGBO(0, 0, 0, 1),
-              fontFamily: 'Reem Kufi Fun',
-              fontSize: 22,
-              letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-              fontWeight: FontWeight.bold,
-              height: 1
+        ),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'LAUNDRY/IRON MANAGER',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Reem Kufi Fun',
+                    fontSize: 22,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.bold,
+                    height: 1),
               ),
-            ),
-            Text('LOGIN', textAlign: TextAlign.left, style: TextStyle(
-              color: Color.fromRGBO(0, 0, 0, 1),
-              fontFamily: 'Reem Kufi Fun',
-              fontSize: 36,
-              letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-              fontWeight: FontWeight.bold,
-              height: 1
-      ),)
-          ]
-      ),
+              Text(
+                'LOGIN',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Reem Kufi Fun',
+                    fontSize: 36,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.bold,
+                    height: 1),
+              )
+            ]),
       ),
     );
   }
 }
 
-//  class StudentLogin extends StatefulWidget {
-//    const StudentLogin({Key? key}) : super(key: key);
+class StudentLogin extends StatelessWidget {
+  StudentLogin({super.key});
 
-//    @override
-//    Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         image: DecorationImage(
-//             image: AssetImage('assets/login.png'), fit: BoxFit.cover),
-//       ),
-//       child: Scaffold(
-//         backgroundColor: Colors.transparent,
-//         body: Stack(
-//           children: [
-//             Container(),
-//             Container(
-//               padding: EdgeInsets.only(left: 35, top: 130),
-//               child: Text(
-//                 'Welcome\nBack',
-//                 style: TextStyle(color: Colors.white, fontSize: 33),
-//               ),
-//             ),
-//             SingleChildScrollView(
-//               child: Container(
-//                 padding: EdgeInsets.only(
-//                     top: MediaQuery.of(context).size.height * 0.5),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Container(
-//                       margin: EdgeInsets.only(left: 35, right: 35),
-//                       child: Column(
-//                         children: [
-//                           TextField(
-//                             style: TextStyle(color: Colors.black),
-//                             decoration: InputDecoration(
-//                                 fillColor: Colors.grey.shade100,
-//                                 filled: true,
-//                                 hintText: "Email",
-//                                 border: OutlineInputBorder(
-//                                   borderRadius: BorderRadius.circular(10),
-//                                 )),
-//                           ),
-//                           SizedBox(
-//                             height: 30,
-//                           ),
-//                           TextField(
-//                             style: TextStyle(),
-//                             obscureText: true,
-//                             decoration: InputDecoration(
-//                                 fillColor: Colors.grey.shade100,
-//                                 filled: true,
-//                                 hintText: "Password",
-//                                 border: OutlineInputBorder(
-//                                   borderRadius: BorderRadius.circular(10),
-//                                 )),
-//                           ),
-//                           SizedBox(
-//                             height: 40,
-//                           ),
-//                           Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                             children: [
-//                               Text(
-//                                 'Sign in',
-//                                 style: TextStyle(
-//                                     fontSize: 27, fontWeight: FontWeight.w700),
-//                               ),
-//                               CircleAvatar(
-//                                 radius: 30,
-//                                 backgroundColor: Color(0xff4c505b),
-//                                 child: IconButton(
-//                                     color: Colors.white,
-//                                     onPressed: () {},
-//                                     icon: Icon(
-//                                       Icons.arrow_forward,
-//                                     )),
-//                               )
-//                             ],
-//                           ),
-//                           SizedBox(
-//                             height: 40,
-//                           ),
-//                           Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                             children: [
-//                               TextButton(
-//                                 onPressed: () {
-//                                   Navigator.pushNamed(context, 'register');
-//                                 },
-                                
-                              
-//                             ],
-//                           )
-//                         ],
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
+  FocusNode emailInputNode = FocusNode();
+  FocusNode passwordInputNode = FocusNode();
+
+    String? validateEmail(String value) {
+      String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?)*$";
+      RegExp regex = RegExp(pattern);
+      if (!regex.hasMatch(value) || value == null) {
+        return 'Enter a valid email address';
+      } else {
+        return null;
+      }
+    }
+
+    // ignore: unused_element
+    String? validatePassword(String value) {
+      String pattern = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+      RegExp regex = RegExp(pattern);
+      if (!regex.hasMatch(value) || value == null) {
+        return 'Enter a valid password';
+      } else {
+        return null;
+      }
+    } 
+
+    TextEditingController emailId = TextEditingController();
+    TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    void openKeyboard() {
+      FocusScope.of(context).requestFocus(emailInputNode);
+    }
+
+    return Scaffold(
+        body: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/image/student_bg.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'STUDENT',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontFamily: 'Reem Kufi Fun',
+                        fontSize: 22,
+                        letterSpacing:
+                            0 /*percentages not used in flutter. defaulting to zero*/,
+                        fontWeight: FontWeight.bold,
+                        height: 1),
+                  ),
+                  Text(
+                    'LOGIN',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontFamily: 'Reem Kufi Fun',
+                        fontSize: 36,
+                        letterSpacing:
+                            0 /*percentages not used in flutter. defaulting to zero*/,
+                        fontWeight: FontWeight.bold,
+                        height: 1),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: TextFormField(
+                      controller: emailId,
+                      validator: (value) => validateEmail(value!),
+                      decoration: InputDecoration(
+                        labelText: "Email ID",
+                        border: OutlineInputBorder(),
+                      ),
+                      // focusNode: emailInputNode,
+                      // autofocus: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: TextFormField(
+                      controller: password,
+                      validator: (value) => validateEmail(value!),
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                      ),
+                      // focusNode: passwordInputNode,
+                      // autofocus: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 335,
+                        height: 66,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(12),
+                            topRight: Radius.circular(12),
+                            bottomLeft: Radius.circular(12),
+                            bottomRight: Radius.circular(12),
+                          ),
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        child: GestureDetector(
+                          onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => StudentLogin())),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: const [
+                              Text(
+                                'LOGIN',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  color: Color.fromRGBO(247, 236, 222, 1),
+                                  fontFamily: 'Reem Kufi Fun',
+                                  fontSize: 19,
+                                  letterSpacing:
+                                      1 /*percentages not used in flutter. defaulting to zero*/,
+                                  fontWeight: FontWeight.w400,
+                                  height: 1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ]))
+        // ]
+        );
+  }
+}
 
 // class Drawhorizontalline extends CustomPainter {
 //   Paint _paint;
